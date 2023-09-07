f38a17360a45bb7278b582249868719961d54eb4
unknown <zesco@KINYAMA-ISD-LK.zescocorp.zescoad.zesco.co.zm>
2023-09-07 13:02:36 +0200

    Added ssl certificate

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+var fs = require('fs'),
+    http = require('http'),
+    https = require('https');
 const express = require('express');
 const serveIndex = require('serve-index');
 
+var options = {
+    key: fs.readFileSync('/etc/ssl/myserver.key'),
+    cert: fs.readFileSync('/etc/ssl/star_zesco_co_zm.crt'),
+};
+var port = 3000;
+
 const app = express();
+
+var server = https.createServer(options, app).listen(port, function(){
+    console.log("Express server listening on port " + port);
+  });
 
 app.use((req, res, next) => {
   console.log('Time: ', Date.now());
@@ -20,4 +33,4 @@ app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-app.listen(4000,'0.0.0.0', () => console.log('Example app is listening on port 3000.'));
+//app.listen(4000,'0.0.0.0', () => console.log('Example app is listening on port 3000.'));
